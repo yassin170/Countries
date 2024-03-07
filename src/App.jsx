0942cf969b4aa4e4288bcd7121c32ce4d1af8f0c
yassin170 <arahou.yassin@gmail.com>
2024-03-07 12:29:25 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react';
+import './App.css';
+
+function App() {
+
+  return (
+    <div className='flex justify-center items-center h-screen w-screen text-4xl'>
+      hello
+    </div>
+  )
+}
+
+export default App
